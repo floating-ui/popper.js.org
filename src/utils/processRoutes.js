b5fc99f01bd30896c7eb65e7456d394bd4a9490c
Federico Zivolo <federico.zivolo@gmail.com>
2020-04-27 13:45:25 +0200

    docs: add react-popper docs to website

--- src/utils/processRoutes.js
+++ src/utils/processRoutes.js
@@ -2,11 +2,14 @@ export default function processRoutes(routes, path) {
   const versionMatch = path.match(/^\/docs\/(v[0-9]+)\//);
   const docsVersion = versionMatch ? versionMatch[1] : null;
 
+  const section = path
+    .split('/')
+    .slice(1, 3)
+    .join('/');
+
   const preprocessedRoutes = routes.filter(
     route =>
-      !route.slug.startsWith('/docs/') ||
-      route.slug.startsWith(`/docs/${docsVersion}/`) ||
-      route.slug.match(/^\/docs\/v[0-9]+\/$/)
+      route.slug.split('/').length <= 4 || route.slug.startsWith(`/${section}`)
   );
 
   return preprocessedRoutes;
