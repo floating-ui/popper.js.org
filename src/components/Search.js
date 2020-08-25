8da678d59b243b5574de2e51e472d8cdf0ea77ea
s-pace <sylvain.pace@algolia.com>
2020-08-25 10:09:03 +0200

    refactor(search): remove duplicate

--- src/components/Search.js
+++ src/components/Search.js
@@ -32,11 +32,7 @@ const Search = () => (
           --docsearch-muted-color: rgb(127, 132, 151);
           --docsearch-key-shadow: inset 0 -2px 0 0 rgb(40, 45, 85),
             inset 0 0 1px 1px rgb(81, 87, 125), 0 2px 2px 0 rgba(3, 4, 9, 0.3);
-          --docsearch-footer-background: #2f263c;
-          --docsearch-footer-shadow: inset 0 1px 0 0 rgba(73, 76, 106, 0.5),
             0 -4px 8px 0 rgba(0, 0, 0, 0.2);
-          --docsearch-logo-color: rgb(255, 255, 255);
-          --docsearch-muted-color: rgb(127, 132, 151);
 
           .DocSearch-Button {
             --docsearch-searchbox-background: rgb(235, 237, 240);
